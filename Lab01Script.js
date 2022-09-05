bf8a8d6c371afaf573ab353fca0d3630c10585d8
emilysarro <112663397+emilysarro@users.noreply.github.com>
2022-09-05 10:13:00 -0400

    Update Lab01Script.js

--- Lab01Script.js
+++ Lab01Script.js
@@ -1,14 +1,15 @@
 var array = [];
- runningtotal = 0;
+for (var total = 0; total<=5; total++)
  {var val = math.floor(100*math.random());
  array.push(val);
- runningtotal += val;}
+ total += val;
+ }
  
- mean = runningtotal/array.length;
+ mean = total/array.length;
  
  var outarray = [];
  
- {for array[] <= runningtotal
-	if array[] > mean, outarray[array[]]
+ if (array[total]>mean)
+	greater.push(array[total])
 		
 	}	
