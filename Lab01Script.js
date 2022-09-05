eff8c01d074d05bb817f65a2bd80e70e5275583e
emilysarro <sarroen19@juniata.edu>
2022-09-05 10:07:34 -0400

    Create Lab01Script.js

---
+++ Lab01Script.js
@@ -0,0 +1,14 @@
+var array = [];
+ runningtotal = 0;
+ {var val = math.floor(100*math.random());
+ array.push(val);
+ runningtotal += val;}
+ 
+ mean = runningtotal/array.length;
+ 
+ var outarray = [];
+ 
+ {for array[] <= runningtotal
+	if array[] > mean, outarray[array[]]
+		
+	}	
