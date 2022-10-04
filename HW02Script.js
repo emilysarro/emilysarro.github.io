70edeb41eb00347ec8f48ef282a7db5aa218c7e9
emilysarro <sarroen19@juniata.edu>
2022-10-04 10:40:57 -0400

    updates

--- HW02Script.js
+++ HW02Script.js
@@ -1,28 +1,18 @@
 "use strict";
+var canvas;
 var gl;
 var vertices;
-kochmount();
+var positions;
+var numtimesubdiv = 0;
+var bufferId;
 
-function kochmount(){
 
-var canvas = document.getElementById("gl-canvas");
+function init(){
+
+canvas = document.getElementById("gl-canvas");
 
    gl = canvas.getContext('webgl2');
    if (!gl) alert( "WebGL 2.0 isn't available" );
-
-	vertices = [
-	vec2(-.99,0),
-	vec2(.99,0)];
-
-
-//for (var i=0; positions.length < numPositions; i++){
-//var j = Math.floor(3*Math.random());
-
-//p = add(positions[i], vertices[j]);
-//p = mult(.5,p);
-//positions.push(p);
-
-//}
 
 
 gl.viewport(0, 0, canvas.width, canvas.height);
@@ -31,21 +21,40 @@ gl.viewport(0, 0, canvas.width, canvas.height);
 var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-var bufferId = gl.createBuffer();
+bufferId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(positions), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
 
 
  var positionLoc = gl.getAttribLocation(program, "aPosition");
     gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionLoc);
 
+document.getElementById("slider").onchange = function(event) {
+        numTimesToSubdivide = parseInt(event.target.value);
+        render();
+  };
+
 render();
 };
 
+//function line(a,b){
+//positions.push(a,b);
+//}
+
 
 function render() {
+
+vertices = [
+	vec2(-.99,0.0),
+	vec2(.99,0.0)];
+positions = [];
+ positions.push(vertices);  
+ //positions = [];
+gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(positions));
     gl.clear( gl.COLOR_BUFFER_BIT );
+gl.drawArrays(gl.POINTS, 0,vertices.length);
     gl.drawArrays(gl.LINES, 0,vertices.length);
+    positions = [];
 }
 
