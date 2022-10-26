f2a2e62b0f77e5fd3ef4bb179cc8350608efb8d0
emilysarro <sarroen19@juniata.edu>
2022-10-25 20:44:16 -0400

    homework

--- Lab10ColorCube.js
+++ Lab10ColorCube.js
@@ -3,7 +3,11 @@
 var canvas;
 var gl;
 
-var numPositions  = 45;
+var tParam = 0.0;
+var tLoc;
+var DeltaT = 0.01;
+
+var numPositions  = 144;
 
 var positions = [];
 var colors = [];
@@ -16,6 +20,9 @@ var flag = false;
 var axis = 0;
 var theta = [0, 0, 0];
 
+var delay = 100;
+var morph = true;
+
 var thetaLoc;
 
 init();
@@ -27,7 +34,7 @@ function init()
     gl = canvas.getContext('webgl2');
     if (!gl) alert("WebGL 2.0 isn't available");
 
-colorCube();
+colorObj();
    
 
 
@@ -59,6 +66,19 @@ colorCube();
     gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionLoc);
 
+var iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, iBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(ipositions), gl.STATIC_DRAW);
+
+
+    var ipositionLoc = gl.getAttribLocation(program, "iPosition");
+    gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(ipositionLoc);
+
+tLoc = gl.getUniformLocation(program, "t");
+
+colorLoc = gl.getUniformLocation(program, "inColor" );
+
     thetaLoc = gl.getUniformLocation(program, "uTheta");
 
     //event listeners for buttons
@@ -79,30 +99,88 @@ document.getElementById("TButton").onclick = function(){flag = !flag;};
 }
 
 
-function colorCube()
-{
-    quad(1, 0, 3, 2);
-    quad(2, 3, 7, 6);
-    quad(3, 0, 4, 7);
-    quad(6, 5, 1, 2);
-    quad(4, 5, 6, 7);
-    quad(5, 4, 8, 9);
-	quad(9, 8, 0, 1);
-}
+function colorObj()
 
-function quad(a, b, c, d)
+{
+
+  for (var cubeNum=0; cubeNum<=1; cubeNum++)
+
+    {
+
+      for (var obj=0; obj<=3; obj++)
+
+      {
+//for I
+
+        quad(1+8*obj, 0+8*obj, 3+8*obj, 2+8*obj, cubeNum);
+
+        quad(2+8*obj, 3+8*obj, 7+8*obj, 6+8*obj, cubeNum);
+
+        quad(3+8*obj, 0+8*obj, 4+8*obj, 7+8*obj, cubeNum);
+
+        quad(6+8*obj, 5+8*obj, 1+8*obj, 2+8*obj, cubeNum);
+
+        quad(4+8*obj, 5+8*obj, 6+8*obj, 7+8*obj, cubeNum);
+
+        quad(5+8*obj, 4+8*obj, 0+8*obj, 1+8*obj, cubeNum);
+
+//for U
+
+quad2(1+8*obj, 0+8*obj, 3+8*obj, 2+8*obj, cubeNum);
+
+        quad2(2+8*obj, 3+8*obj, 7+8*obj, 6+8*obj, cubeNum);
+
+        quad2(3+8*obj, 0+8*obj, 4+8*obj, 7+8*obj, cubeNum);
+
+        quad2(6+8*obj, 5+8*obj, 1+8*obj, 2+8*obj, cubeNum);
+
+        quad2(4+8*obj, 5+8*obj, 6+8*obj, 7+8*obj, cubeNum);
+
+        quad2(5+8*obj, 4+8*obj, 0+8*obj, 1+8*obj, cubeNum);
+
+      }
+
+    }
+}
+function quad1(a, b, c, d, cubeNum)
 {
     var vertices = [
+
+	vec4(-0.75, -0.15,  0.75, 1.0),
+        vec4(-0.75,  -0.6,  0.75, 1.0),
+        vec4(0.75,  -0.6,  0.75, 1.0),
+        vec4(0.75, -0.15,  0.75, 1.0),
+        vec4(-0.75, -0.15, 0.5, 1.0),
+        vec4(-0.75,  -0.6, 0.5, 1.0),
+        vec4(0.75,  -0.6, 0.5, 1.0),
+        vec4(0.75, -0.15, 0.5, 1.0),
+
         vec4(-0.15, 0.75,  0.75, 1.0),
-        vec4(-0.15,  -0.6,  0.75, 1.0),
-        vec4(0.5,  -0.6,  0.75, 1.0),
+        vec4(-0.15,  -0.15,  0.75, 1.0),
+        vec4(0.15,  -0.15,  0.75, 1.0),
         vec4(0.15, 0.75,  0.75, 1.0),
         vec4(-0.15, 0.75, 0.5, 1.0),
-        vec4(-0.15,  -0.6, 0.5, 1.0),
-        vec4(0.5,  -0.6, 0.5, 1.0),
+        vec4(-0.15,  -0.15, 0.5, 1.0),
+        vec4(0.15,  -0.15, 0.5, 1.0),
         vec4(0.15, 0.75, 0.5, 1.0),
-vec4(0.5,  -0.3,  0.75, 1.0),
-vec4(0.5,  -0.3,  0.5, 1.0)
+
+	vec4(-0.75, 0.75,  0.75, 1.0),
+        vec4(-0.75,   0.5,  0.75, 1.0),
+        vec4(-0.15,   0.5,  0.75, 1.0),
+        vec4(-0.15, 0.75,  0.75, 1.0),
+        vec4(-0.75, 0.75, 0.5, 1.0),
+        vec4(-0.75,   0.5, 0.5, 1.0),
+        vec4(-0.15,   0.5, 0.5, 1.0),
+        vec4(-0.15, 0.75, 0.5, 1.0),
+
+       vec4(0.75, 0.75,  0.75, 1.0),
+        vec4(0.75,   0.5,  0.75, 1.0),
+        vec4(0.15,   0.5,  0.75, 1.0),
+        vec4(0.15, 0.75,  0.75, 1.0),
+        vec4(0.75, 0.75, 0.5, 1.0),
+        vec4(0.75,   0.5, 0.5, 1.0),
+        vec4(0.15,   0.5, 0.5, 1.0),
+        vec4(0.15, 0.75, 0.5, 1.0)
     ];
 
     var vertexColors = [
@@ -113,8 +191,35 @@ vec4(0.5,  -0.3,  0.5, 1.0)
         vec4(0.0, 0.0, 1.0, 1.0),  // blue
         vec4(1.0, 0.0, 1.0, 1.0),  // magenta
 	//vec4(1.0, 1.0, 1.0, 1.0),   // white
-        vec4(0.0, 1.0, 1.0, 1.0),  // cyan
-         vec4(0.0, 1.0, 1.0, 1.0),  // cyan
+vec4(0.0, 1.0, 1.0, 1.0),  // cyan
+
+vec4(0.0, 0.0, 0.0, 1.0),  // black
+        vec4(1.0, 0.0, 0.0, 1.0),  // red
+        vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+        vec4(0.0, 1.0, 0.0, 1.0),  // green
+        vec4(0.0, 0.0, 1.0, 1.0),  // blue
+        vec4(1.0, 0.0, 1.0, 1.0),  // magenta
+	//vec4(1.0, 1.0, 1.0, 1.0),   // white
+vec4(0.0, 1.0, 1.0, 1.0),  // cyan
+
+vec4(0.0, 0.0, 0.0, 1.0),  // black
+        vec4(1.0, 0.0, 0.0, 1.0),  // red
+        vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+        vec4(0.0, 1.0, 0.0, 1.0),  // green
+        vec4(0.0, 0.0, 1.0, 1.0),  // blue
+        vec4(1.0, 0.0, 1.0, 1.0),  // magenta
+	//vec4(1.0, 1.0, 1.0, 1.0),   // white
+vec4(0.0, 1.0, 1.0, 1.0),  // cyan
+
+vec4(0.0, 0.0, 0.0, 1.0),  // black
+        vec4(1.0, 0.0, 0.0, 1.0),  // red
+        vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+        vec4(0.0, 1.0, 0.0, 1.0),  // green
+        vec4(0.0, 0.0, 1.0, 1.0),  // blue
+        vec4(1.0, 0.0, 1.0, 1.0),  // magenta
+	//vec4(1.0, 1.0, 1.0, 1.0),   // white
+vec4(0.0, 1.0, 1.0, 1.0),  // cyan
+      
     ];
 
     // We need to parition the quad into two triangles in order for
@@ -135,6 +240,107 @@ vec4(0.5,  -0.3,  0.5, 1.0)
     }
 }
 
+//u object
+
+function quad2(a, b, c, d, cubeNum)
+{
+    var vertices2 = [
+
+	vec4(-0.75, -0.15,  0.75, 1.0),
+        vec4(-0.75,  -0.6,  0.75, 1.0),
+        vec4(0.75,  -0.6,  0.75, 1.0),
+        vec4(0.75, -0.15,  0.75, 1.0),
+        vec4(-0.75, -0.15, 0.5, 1.0),
+        vec4(-0.75,  -0.6, 0.5, 1.0),
+        vec4(0.75,  -0.6, 0.5, 1.0),
+        vec4(0.75, -0.15, 0.5, 1.0),
+
+        vec4(-0.15, -0.15,  0.75, 1.0),
+        vec4(-0.15,  -0.15,  0.75, 1.0),
+        vec4(0.15,  -0.15,  0.75, 1.0),
+        vec4(0.15, -0.15,  0.75, 1.0),
+        vec4(-0.15,-0.15, 0.5, 1.0),
+        vec4(-0.15,  -0.15, 0.5, 1.0),
+        vec4(0.15,  -0.15, 0.5, 1.0),
+        vec4(0.15, -0.15, 0.5, 1.0),
+
+	vec4(-0.75, 0.75,  0.75, 1.0),
+        vec4(-0.75,  -0.15,  0.75, 1.0),
+        vec4(-0.15,   -0.15,  0.75, 1.0),
+        vec4(-0.15, 0.75,  0.75, 1.0),
+        vec4(-0.75, 0.75, 0.5, 1.0),
+        vec4(-0.75,   -0.15, 0.5, 1.0),
+        vec4(-0.15,   -0.15, 0.5, 1.0),
+        vec4(-0.15, 0.75, 0.5, 1.0),
+
+       vec4(0.75, 0.75,  0.75, 1.0),
+        vec4(0.75,   -0.15,  0.75, 1.0),
+        vec4(0.15,   -0.15,  0.75, 1.0),
+        vec4(0.15, 0.75,  0.75, 1.0),
+        vec4(0.75, 0.75, 0.5, 1.0),
+        vec4(0.75,   -0.15, 0.5, 1.0),
+        vec4(0.15,   -0.15, 0.5, 1.0),
+        vec4(0.15, 0.75, 0.5, 1.0)
+    ];
+
+    var vertexColors2 = [
+       vec4(0.0, 0.0, 0.0, 1.0),  // black
+        vec4(1.0, 0.0, 0.0, 1.0),  // red
+        vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+        vec4(0.0, 1.0, 0.0, 1.0),  // green
+        vec4(0.0, 0.0, 1.0, 1.0),  // blue
+        vec4(1.0, 0.0, 1.0, 1.0),  // magenta
+	//vec4(1.0, 1.0, 1.0, 1.0),   // white
+vec4(0.0, 1.0, 1.0, 1.0),  // cyan
+
+vec4(0.0, 0.0, 0.0, 1.0),  // black
+        vec4(1.0, 0.0, 0.0, 1.0),  // red
+        vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+        vec4(0.0, 1.0, 0.0, 1.0),  // green
+        vec4(0.0, 0.0, 1.0, 1.0),  // blue
+        vec4(1.0, 0.0, 1.0, 1.0),  // magenta
+	//vec4(1.0, 1.0, 1.0, 1.0),   // white
+vec4(0.0, 1.0, 1.0, 1.0),  // cyan
+
+vec4(0.0, 0.0, 0.0, 1.0),  // black
+        vec4(1.0, 0.0, 0.0, 1.0),  // red
+        vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+        vec4(0.0, 1.0, 0.0, 1.0),  // green
+        vec4(0.0, 0.0, 1.0, 1.0),  // blue
+        vec4(1.0, 0.0, 1.0, 1.0),  // magenta
+	//vec4(1.0, 1.0, 1.0, 1.0),   // white
+vec4(0.0, 1.0, 1.0, 1.0),  // cyan
+
+vec4(0.0, 0.0, 0.0, 1.0),  // black
+        vec4(1.0, 0.0, 0.0, 1.0),  // red
+        vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+        vec4(0.0, 1.0, 0.0, 1.0),  // green
+        vec4(0.0, 0.0, 1.0, 1.0),  // blue
+        vec4(1.0, 0.0, 1.0, 1.0),  // magenta
+	//vec4(1.0, 1.0, 1.0, 1.0),   // white
+vec4(0.0, 1.0, 1.0, 1.0),  // cyan
+      
+    ];
+
+    // We need to parition the quad into two triangles in order for
+    // WebGL to be able to render it.  In this case, we create two
+    // triangles from the quad indices
+
+    //vertex color assigned by the index of the vertex
+
+    var indices = [a, b, c, a, c, d];
+
+    for ( var i = 0; i < indices.length; ++i ) {
+       positions.push( vertices2[indices[i]] );
+       // colors.push( vertexColors2[indices[i]] );
+
+
+        // for solid colored faces use
+        colors.push(vertexColors2[a]);
+    }
+}
+
+
 function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -142,7 +348,18 @@ function render()
     if (flag) theta[axis] += 2.0;
     gl.uniform3fv(thetaLoc, theta);
 
+if (morph) tParam += DeltaT;
+	if (tParam>=1.0 || tParam<= 0.0)
+	DeltaT = -DeltaT;
+	gl.uniform1f(tLoc, tParam);
+
     gl.drawArrays(gl.TRIANGLES, 0, numPositions);
 gl.drawArrays(gl.LINES, numPositions, 6);
     requestAnimationFrame(render);
+
+setTimeout(
+	function (){
+requestAnimationFrame(render);
+}, delay);
+
 }
